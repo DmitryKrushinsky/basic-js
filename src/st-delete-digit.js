fdec1be98b5a3581a9fca7125c692b428a16b5cc
DmitryKrushinsky <dima.mysteri.98@gmail.com>
2021-09-28 18:55:41 +0300

    feat: Add solutions for 5 tasks

--- src/st-delete-digit.js
+++ src/st-delete-digit.js
@@ -11,7 +11,8 @@ import { NotImplementedError } from '../extensions/index.js';
  * For n = 152, the output should be 52
  *
  */
-export default function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function deleteDigit(n) {
+  return parseInt(n.toString().replace(Math.min.apply(null, n.toString().split('').map(el => parseInt(el))).toString(), ''));
+  
 }
+  
