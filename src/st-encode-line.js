d04f8348d2a43dfe8e47a901dc8c4e5932fe40b9
DmitryKrushinsky <dima.mysteri.98@gmail.com>
2021-10-03 23:14:05 +0300

    feat: Add solution for encode line task

--- src/st-encode-line.js
+++ src/st-encode-line.js
@@ -8,9 +8,28 @@ import { NotImplementedError } from '../extensions/index.js';
  *
  * @example
  * For aabbbc should return 2a3bc
+ * 
+ * let countLetters = str.split('').reduce((acc, el) => {
+    if (!acc[el]) {
+      acc[el] = 1;
+    }else {
+      acc[el] += 1;
+    }
+    return acc;
+  }, {});
+  return Object.entries(countLetters).map(el => (el[1] === 1 ? '' : el[1]) + el[0]).join('')
  *
  */
-export default function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function encodeLine(str) {
+  let result = '';
+  let countLetters = 1;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i + 1]) {
+      countLetters++;
+    } else if (str[i] !== str[i + 1]) {
+        result += countLetters !== 1 ? countLetters + str[i] : str[i];
+        countLetters = 1;
+      }
+}
+return result;
 }
